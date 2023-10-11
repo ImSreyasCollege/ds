d7fc4aeec0af2c613aa42da7c2172d4325373739
ImSreyasCollege <sreyassatheesh755@gmail.com>
2023-10-11 16:00:47 +0530

    code refactor in the search program

--- cycle 1/search.c
+++ cycle 1/search.c
@@ -1,65 +1,82 @@
 #include <stdio.h>
-
-// Function for linear search
-int linearSearch(int arr[], int n, int key) {
+// This function is used for getting the search key 
+int getKey(){
+    int key;
+    printf("enter the key to search : ");
+    scanf("%d", &key);
+    return key;
+}
+// function to perform linear search
+void linearSearch(int n) {
+    int arr[50], flag=0; 
+    printf("Enter elements of the array :\n");
     for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int key = getKey();
+    int i=0;
+    for (i = 0; i < n; i++) {
         if (arr[i] == key) {
-            return i; // Element found, return its index
+            flag = 1;
+            break;
         }
     }
-    return -1; // Element not found
+    if(flag==1){
+        printf("element found at location %d.\n", i+1);
+    } else {
+        printf("element not found.\n");
+    }
 }
-
-// Function for binary search (assuming the array is sorted in ascending order)
-int binarySearch(int arr[], int n, int key) {
-    int left = 0;
+// function to perform binary search 
+void binarySearch(int n) {
+    int left = 0, arr[50];
     int right = n - 1;
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-
-        if (arr[mid] == key) {
-            return mid; // Element found, return its index
-        } else if (arr[mid] < key) {
-            left = mid + 1; // Search the right half
-        } else {
-            right = mid - 1; // Search the left half
-        }
-    }
-
-    return -1; // Element not found
-}
-
-int main() {
-    int arr[100];
-    int n, key;
-
-    printf("Enter the size of the array: ");
-    scanf("%d", &n);
 
     printf("Enter elements of the array in ascending order:\n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+    int key = getKey();
+    int flag = 0;
+    int mid = 0;
+    while (left <= right) {
+        mid = left + (right - left) / 2;
 
-    printf("Enter the element to search for: ");
-    scanf("%d", &key);
-
-    // Perform linear search
-    int linearResult = linearSearch(arr, n, key);
-    if (linearResult != -1) {
-        printf("Linear Search: Element found at position %d.\n", linearResult+1);
-    } else {
-        printf("Linear Search: Element not found.\n");
+        if (arr[mid] == key) {
+            flag = 1;
+            break;
+        } else if (arr[mid] < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
-
-    // Perform binary search
-    int binaryResult = binarySearch(arr, n, key);
-    if (binaryResult != -1) {
-        printf("Binary Search: Element found at position %d.\n", binaryResult+1);
+    if(flag==1){
+        printf("element found at location %d.\n", mid+1);
     } else {
-        printf("Binary Search: Element not found.\n");
+        printf("element not found.\n");
     }
+}
 
-    return 0;
+int main() {
+    int arr[100];
+    int n;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int opt;
+    while(1){
+        printf("1.binary search\n2.linear search\n3.exit\nEnter the operation : ");
+        scanf("%d", &opt);
+        switch(opt){
+            case 1: binarySearch(n);
+            break;
+            case 2: linearSearch(n);
+            break;
+            case 3: return 1;
+            default: printf("invalid input.\n");
+        }
+    }
+    return 1;
 }
