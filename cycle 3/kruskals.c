89727d6ab2918ca97042682d287934d3eacd5c50
ImSreyas <sreyassatheesh755@gmail.com>
2023-11-26 15:38:19 +0530

    ss of kruskals algo is added

--- cycle 3/kruskals.c
+++ cycle 3/kruskals.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Comparator function to use in sorting
 int comparator(const void* p1, const void* p2) {
     const int(*x)[3] = p1;
     const int(*y)[3] = p2;
-
     return (*x)[2] - (*y)[2];
 }
 
-// Initialization of parent[] and rank[] arrays
 void makeSet(int parent[], int rank[], int n) {
     for (int i = 0; i < n; i++) {
         parent[i] = i;
@@ -17,86 +14,56 @@ void makeSet(int parent[], int rank[], int n) {
     }
 }
 
-// Function to find the parent of a node
 int findParent(int parent[], int component) {
     if (parent[component] == component)
         return component;
-
     return parent[component] = findParent(parent, parent[component]);
 }
 
-// Function to unite two sets
 void unionSet(int u, int v, int parent[], int rank[], int n) {
-    // Finding the parents
     u = findParent(parent, u);
     v = findParent(parent, v);
-
     if (rank[u] < rank[v]) {
         parent[u] = v;
     } else if (rank[u] > rank[v]) {
         parent[v] = u;
     } else {
         parent[v] = u;
-
-        // Since the rank increases if
-        // the ranks of two sets are same
         rank[u]++;
     }
 }
 
-// Function to find the MST
 void kruskalAlgo(int n, int edge[][3]) {
-    // First, we sort the edge array in ascending order
-    // so that we can access minimum distances/cost
     qsort(edge, n, sizeof(edge[0]), comparator);
-
     int parent[n];
     int rank[n];
-
-    // Function to initialize parent[] and rank[]
     makeSet(parent, rank, n);
-
-    // To store the minimum cost
     int minCost = 0;
-
     printf("Following are the edges in the constructed MST\n");
     for (int i = 0; i < n; i++) {
         int v1 = findParent(parent, edge[i][0]);
         int v2 = findParent(parent, edge[i][1]);
         int wt = edge[i][2];
-
-        // If the parents are different, that
-        // means they are in different sets, so
-        // union them
         if (v1 != v2) {
             unionSet(v1, v2, parent, rank, n);
             minCost += wt;
-            printf("%d -- %d == %d\n", edge[i][0],
-                   edge[i][1], wt);
+            printf("%d -- %d == %d\n", edge[i][0], edge[i][1], wt);
         }
     }
-
     printf("Minimum Cost Spanning Tree: %d\n", minCost);
 }
 
-// Driver code
 int main() {
     int vertices, edges;
-
     printf("Enter the number of vertices: ");
     scanf("%d", &vertices);
-
     printf("Enter the number of edges: ");
     scanf("%d", &edges);
-
     int edge[edges][3];
-
     printf("Enter the edges (src dest weight):\n");
     for (int i = 0; i < edges; i++) {
         scanf("%d %d %d", &edge[i][0], &edge[i][1], &edge[i][2]);
     }
-
     kruskalAlgo(edges, edge);
-
     return 0;
 }
