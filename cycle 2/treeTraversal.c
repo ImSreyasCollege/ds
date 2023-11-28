da8c40f3b1a34a50979c24950ba016166db5ef3d
ImSreyasCollege <sreyassatheesh755@gmail.com>
2023-11-28 10:50:44 +0530

    cycle 1 and 2 output taken

--- cycle 2/treeTraversal.c
+++ cycle 2/treeTraversal.c
@@ -1,94 +1,152 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure for a binary tree node
-struct TreeNode {
+struct Node 
+{
     int data;
-    struct TreeNode* left;
-    struct TreeNode* right;
+    struct Node* left;
+    struct Node* right;
 };
 
-// Function to create a new tree node
-struct TreeNode* createNode(int value) {
-    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+
+struct Node* createNode(int value) 
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = value;
-    newNode->left = NULL;
-    newNode->right = NULL;
+    newNode->left = newNode->right = NULL;
     return newNode;
 }
 
-// Function to perform in-order traversal (Left-Root-Right)
-void inOrderTraversal(struct TreeNode* root) {
-    if (root != NULL) {
+
+struct Node* insert(struct Node* root, int value) 
+{
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+
+void inOrderTraversal(struct Node* root) 
+{
+    if (root != NULL) 
+    {
         inOrderTraversal(root->left);
         printf("%d ", root->data);
         inOrderTraversal(root->right);
     }
 }
 
-// Function to perform pre-order traversal (Root-Left-Right)
-void preOrderTraversal(struct TreeNode* root) {
-    if (root != NULL) {
+
+void preOrderTraversal(struct Node* root) 
+{
+    if (root != NULL)
+     {
         printf("%d ", root->data);
         preOrderTraversal(root->left);
         preOrderTraversal(root->right);
     }
 }
 
-// Function to perform post-order traversal (Left-Right-Root)
-void postOrderTraversal(struct TreeNode* root) {
-    if (root != NULL) {
+void postOrderTraversal(struct Node* root) 
+{
+    if (root != NULL) 
+    {
+    
         postOrderTraversal(root->left);
         postOrderTraversal(root->right);
         printf("%d ", root->data);
     }
 }
 
-// Function to perform breadth-first search (level order traversal)
-void breadthFirstSearch(struct TreeNode* root) {
+
+void breadthFirstSearch(struct Node* root)
+ {
     if (root == NULL)
         return;
 
-    struct TreeNode* queue[100]; // Assuming a maximum of 100 nodes in the tree
-    int front = 0, rear = 0;
-    queue[rear++] = root;
+    struct Node* queue[100];
+    int front = -1, rear = -1;
 
-    while (front < rear) {
-        struct TreeNode* current = queue[front++];
+    queue[++rear] = root;
+
+    while (front < rear)
+     {
+        struct Node* current = queue[++front];
         printf("%d ", current->data);
 
-        if (current->left != NULL) {
-            queue[rear++] = current->left;
-        }
-        if (current->right != NULL) {
-            queue[rear++] = current->right;
-        }
+        if (current->left != NULL)
+            queue[++rear] = current->left;
+
+        if (current->right != NULL)
+            queue[++rear] = current->right;
     }
 }
 
-int main() {
-    // Creating a simple binary tree
-    struct TreeNode* root = createNode(1);
-    root->left = createNode(2);
-    root->right = createNode(3);
-    root->left->left = createNode(4);
-    root->left->right = createNode(5);
+int main() 
+{
+    struct Node* root = NULL;
+    int choice, value;
 
-    printf("In-Order Traversal: ");
-    inOrderTraversal(root);
-    printf("\n");
+    do 
+    {
+        printf("\nBinary Tree Operations:\n");
+        printf("1. Insert\n");
+        printf("2. In-order Traversal\n");
+        printf("3. Pre-order Traversal\n");
+        printf("4. Post-order Traversal\n");
+        printf("5. Breadth-First Search (BFS)\n");
+        printf("6. Quit\n");
 
-    printf("Pre-Order Traversal: ");
-    preOrderTraversal(root);
-    printf("\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    printf("Post-Order Traversal: ");
-    postOrderTraversal(root);
-    printf("\n");
+        switch (choice)
+         {
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &value);
+                root = insert(root, value);
+                break;
 
-    printf("Breadth-First Search: ");
-    breadthFirstSearch(root);
-    printf("\n");
+            case 2:
+                printf("In-order Traversal: ");
+                inOrderTraversal(root);
+                printf("\n");
+                break;
 
+            case 3:
+                printf("Pre-order Traversal: ");
+                preOrderTraversal(root);
+                printf("\n");
+                break;
+
+            case 4:
+                printf("Post-order Traversal: ");
+                postOrderTraversal(root);
+                printf("\n");
+                break;
+
+            case 5:
+                printf("Breadth-First Search (BFS): ");
+                breadthFirstSearch(root);
+                printf("\n");
+                break;
+
+            case 6:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice. Please try again.\n");
+                break;
+        }
+    } 
+    while (choice != 6);
     return 0;
 }
--- cycle 3/binomialHeap.c
+++ cycle 3/binomialHeap.c
@@ -1,207 +1,374 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#include<stdio.h>
+#include<malloc.h>
 
-// Node structure for Binomial Heap
-struct Node {
-    int data, degree;
-    struct Node *parent, *child, *sibling;
+struct node 
+{
+    int n;
+    int degree;
+    struct node* parent;
+    struct node* child;
+    struct node* sibling;
 };
 
-// Create a new node with given key
-struct Node *createNode(int key) {
-    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-    newNode->data = key;
-    newNode->degree = 0;
-    newNode->parent = newNode->child = newNode->sibling = NULL;
-    return newNode;
+struct node* make_bin_heap();
+int bin_link(struct node*, struct node*);
+struct node* create_node(int);
+struct node* bin_heap_union(struct node*, struct node*);
+struct node* bin_heap_insert(struct node*, struct node*);
+struct node* bin_heap_merge(struct node*, struct node*);
+struct node* bin_heap_extract_min(struct node*);
+int revert_list(struct node*);
+int display(struct node*);
+struct node* find_node(struct node*, int);
+int bin_heap_decrease_key(struct node*, int, int);
+int bin_heap_delete(struct node*, int);
+int count = 1;
+
+struct node* make_bin_heap() 
+{
+    struct node* np;
+    np = NULL;
+    return np;
 }
 
-// Merge two binomial trees of the same degree
-struct Node *mergeBinomialTrees(struct Node *tree1, struct Node *tree2) {
-    if (tree1->data > tree2->data) {
-        struct Node *temp = tree1;
-        tree1 = tree2;
-        tree2 = temp;
-    }
-
-    tree2->parent = tree1;
-    tree2->sibling = tree1->child;
-    tree1->child = tree2;
-    tree1->degree++;
-
-    return tree1;
+struct node * H = NULL;
+struct node *Hr = NULL;
+ 
+int bin_link(struct node* y, struct node* z) 
+{
+    y->parent = z;
+    y->sibling = z->child;
+    z->child = y;
+    z->degree = z->degree + 1;
 }
 
-// Union of two Binomial Heaps
-struct Node *unionBinomialHeaps(struct Node *h1, struct Node *h2) {
-    struct Node *head = NULL;
-    struct Node *temp1 = h1;
-    struct Node *temp2 = h2;
+struct node* create_node(int k) 
+{
+    struct node* p;
+    p = (struct node*) malloc(sizeof(struct node));
+    p->n = k;
+    return p;
+}
 
-    while (temp1 != NULL || temp2 != NULL) {
-        struct Node *next = NULL;
-
-        if (temp1 != NULL && (temp2 == NULL || temp1->degree <= temp2->degree)) {
-            next = temp1;
-            temp1 = temp1->sibling;
-        } else {
-            next = temp2;
-            temp2 = temp2->sibling;
+struct node* bin_heap_union(struct node* H1, struct node* H2) 
+{
+    struct node* prev_x;
+    struct node* next_x;
+    struct node* x;
+    struct node* H = make_bin_heap();
+    H = bin_heap_merge(H1, H2);
+    if (H == NULL)
+        return H;
+    prev_x = NULL;
+    x = H;
+    next_x = x->sibling;
+    while (next_x != NULL) 
+    {
+        if ((x->degree != next_x->degree) || ((next_x->sibling != NULL)
+                && (next_x->sibling)->degree == x->degree)) 
+                {
+            prev_x = x;
+            x = next_x;
+        	} 
+        else 
+        {
+            if (x->n <= next_x->n) 
+            {
+                x->sibling = next_x->sibling;
+                bin_link(next_x, x);
+            } 
+            else 
+            {
+                if (prev_x == NULL)
+                    H = next_x;
+                else
+                    prev_x->sibling = next_x;
+                bin_link(x, next_x);
+                x = next_x;
+            }
         }
+        next_x = x->sibling;
+    }
+    return H;
+}
+ 
+struct node* bin_heap_insert(struct node* H, struct node* x) 
+{
+    struct node* H1 = make_bin_heap();
+    x->parent = NULL;
+    x->child = NULL;
+    x->sibling = NULL;
+    x->degree = 0;
+    H1 = x;
+    H = bin_heap_union(H, H1);
+    return H;
+}
 
-        if (head == NULL) {
-            head = next;
-        } else {
-            head = mergeBinomialTrees(head, next);
+struct node* bin_heap_merge(struct node* H1, struct node* H2) 
+{
+    struct node* H = make_bin_heap();
+    struct node* y;
+    struct node* z;
+    struct node* a;
+    struct node* b;
+    y = H1;
+    z = H2;
+    if (y != NULL) 
+    {
+        if (z != NULL && y->degree <= z->degree)
+            H = y;
+        else if (z != NULL && y->degree > z->degree)
+            /* need some modifications here;the first and the else conditions
+            can be merged together!!!! */
+            H = z;
+        else
+            H = y;
+    } else
+        H = z;
+    while (y != NULL && z != NULL) 
+    {
+        if (y->degree < z->degree) 
+        {
+            y = y->sibling;
+        } 
+        else if (y->degree == z->degree) 
+        {
+            a = y->sibling;
+            y->sibling = z;
+            y = a;
+        } else 
+        {
+            b = z->sibling;
+            z->sibling = y;
+            z = b;
         }
     }
-
-    return head;
+    return H;
 }
 
-// Insert a key into a Binomial Heap
-struct Node *insertNode(struct Node *head, int key) {
-    struct Node *newNode = createNode(key);
-    return unionBinomialHeaps(head, newNode);
+int display(struct node* H) 
+{
+    struct node* p;
+    if (H == NULL) 
+    {
+        printf("\nheap empty");
+        return 0;
+    }
+    printf("\nthe root nodes are:-\n");
+    p = H;
+    while (p != NULL) 
+    {
+        printf("%d", p->n);
+        if (p->sibling != NULL)
+            printf("-->");
+        p = p->sibling;
+    }
+    printf("\n");
 }
 
-// Get the minimum node in a Binomial Heap
-struct Node *getMin(struct Node *heap) {
-    struct Node *minNode = heap;
-    struct Node *temp = heap;
-
-    while (temp != NULL) {
-        if (temp->data < minNode->data) {
-            minNode = temp;
+struct node* bin_heap_extract_min(struct node* H1) 
+{
+    int min;
+    struct node* t = NULL;
+    struct node* x = H1;
+    struct node *Hr;
+    struct node* p;
+    Hr = NULL;
+    if (x == NULL) 
+    {
+        printf("\nnothing to extract");
+        return x;
+    }
+    p = x;
+    while (p->sibling != NULL) 
+    {
+        if ((p->sibling)->n < min) 
+        {
+            min = (p->sibling)->n;
+            t = p;
+            x = p->sibling;
         }
-        temp = temp->sibling;
+        p = p->sibling;
     }
-
-    return minNode;
+    if (t == NULL && x->sibling == NULL)
+        H1 = NULL;
+    else if (t == NULL)
+        H1 = x->sibling;
+    else if (t->sibling == NULL)
+        t = NULL;
+    else
+        t->sibling = x->sibling;
+    if (x->child != NULL) 
+    {
+        revert_list(x->child);
+        (x->child)->sibling = NULL;
+    }
+    H = bin_heap_union(H1, Hr);
+    return x;
 }
-
-// Extract the minimum node from a Binomial Heap
-struct Node *extractMin(struct Node *heap) {
-    struct Node *minNode = getMin(heap);
-    struct Node *prev = NULL, *curr = heap;
-
-    while (curr != minNode) {
-        prev = curr;
-        curr = curr->sibling;
-    }
-
-    if (prev == NULL) {
-        heap = curr->sibling;
-    } else {
-        prev->sibling = curr->sibling;
-    }
-
-    struct Node *child = minNode->child;
-    while (child != NULL) {
-        struct Node *nextChild = child->sibling;
-        child->parent = NULL;
-        child->sibling = NULL;
-        heap = unionBinomialHeaps(heap, child);
-        child = nextChild;
-    }
-
-    free(minNode);
-
-    return heap;
-}
-
-// Decrease the key to a new value
-void decreaseKey(struct Node *heap, int oldKey, int newKey) {
-    // Implementation of decreaseKey depends on your specific requirements
-    // It is not included here for brevity
-    // Typically involves finding the node with oldKey and updating its value
-}
-
-// Delete a key from a Binomial Heap
-struct Node *deleteKey(struct Node *heap, int key) {
-    if (heap == NULL) {
-        printf("Heap is empty.\n");
-        return heap;
-    }
-
-    // Decrease the key to a very small value
-    decreaseKey(heap, key, INT_MIN);
-
-    // Extract the minimum key (which is now at the root)
-    heap = extractMin(heap);
-
-    printf("Key %d deleted successfully.\n", key);
-
-    return heap;
-}
-
-// Display the Binomial Heap in a readable format
-void display(struct Node *root, int depth) {
-    while (root != NULL) {
-        printf("(%d,%d) ", root->data, root->degree);
-        display(root->child, depth + 1);
-        root = root->sibling;
+ 
+int revert_list(struct node* y) 
+{
+    if (y->sibling != NULL) 
+    {
+        revert_list(y->sibling);
+        (y->sibling)->sibling = y;
+    } else 
+    {
+        Hr = y;
     }
 }
 
-// Main function to demonstrate Binomial Heap operations
-int main() {
-    struct Node *heap = NULL;
-    int choice, key;
+struct node* find_node(struct node* H, int k) 
+{
+    struct node* x = H;
+    struct node* p = NULL;
+        if (x->n == k) 
+        {
+        p = x;
+        return p;
+    }
+    if (x->child != NULL && p == NULL) 
+    {
+        p = find_node(x->child, k);
+    }
+ 
+    if (x->sibling != NULL && p == NULL) 
+    {
+        p = find_node(x->sibling, k);
+    }
+    return p;
+}
+ 
+int bin_heap_decrease_key(struct node* H, int i, int k) 
+{
+    int temp;
+    struct node* p;
+    struct node* y;
+    struct node* z;
+    p = find_node(H, i);
+    if (p == NULL) 
+    {
+        printf("\ninvalid choice of key to be reduced");
+        return 0;
+    }
+    if (k > p->n) 
+    {
+        printf("\nsorry!the new key is greater than current one");
+        return 0;
+    }
+    p->n = k;
+    y = p;
+    z = p->parent;
+    while (z != NULL && y->n < z->n) 
+    {
+        temp = y->n;
+        y->n = z->n;
+        z->n = temp;
+        y = z;
+        z = z->parent;
+    }
+    printf("\nkey reduced successfully!");
+}
 
-    do {
-        printf("\n------ Binomial Heap Operations ------\n");
-        printf("1. Insert\n");
-        printf("2. Extract Min\n");
-        printf("3. Delete\n");
-        printf("4. Display\n");
-        printf("5. Quit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                printf("Enter key to insert: ");
-                scanf("%d", &key);
-                heap = insertNode(heap, key);
-                printf("Key %d inserted successfully.\n", key);
-                break;
-
-            case 2:
-                if (heap == NULL) {
-                    printf("Heap is empty.\n");
-                } else {
-                    heap = extractMin(heap);
-                    printf("Minimum element extracted.\n");
-                }
-                break;
-
-            case 3:
-                printf("Enter key to delete: ");
-                scanf("%d", &key);
-                heap = deleteKey(heap, key);
-                break;
-
-            case 4:
-                if (heap == NULL) {
-                    printf("Heap is empty.\n");
-                } else {
-                    printf("Binomial Heap: ");
-                    display(heap, 0);
-                    printf("\n");
-                }
-                break;
-
-            case 5:
-                printf("Quitting the program.\n");
-                break;
-
-            default:
-                printf("Invalid choice. Please enter a valid option.\n");
-        }
-
-    } while (choice != 5);
-
-    return 0;
+int bin_heap_delete(struct node* H, int k) 
+{
+    struct node* np;
+    if (H == NULL) 
+    {
+        printf("\nheap empty");
+        return 0;
+    }
+ 
+    bin_heap_decrease_key(H, k, -1000);
+    np = bin_heap_extract_min(H);
+    if (np != NULL)
+        printf("\nnode deleted successfully");
+}
+ 
+int main() 
+{
+    int i, n, m, l;
+    struct node* p;
+    struct node* np;
+    char ch;
+    printf("\nenter the number of elements:");
+    scanf("%d", &n);
+    printf("\nenter the elements:\n");
+    for (i = 1; i <= n; i++) 
+    {
+        scanf("%d", &m);
+        np = create_node(m);
+        H = bin_heap_insert(H, np);
+    }
+    display(H);
+    do 
+    {
+        printf("\nmenu:-\n");
+        printf(
+                "\n1)insert an element\n2)extract the minimum key node\n3)decrease a node key\n4)delete a node\n5)quit\n");
+        scanf("%d", &l);
+        switch (l) 
+        {
+        case 1:
+            do 
+            {
+                printf("\nenter the element to be inserted:");
+                scanf("%d", &m);
+                p = create_node(m);
+                H = bin_heap_insert(H, p);
+                printf("\nnow the heap is:\n");
+                display(H);
+                printf("\ninsert more(y/y)= \n");
+                fflush(stdin);
+                scanf("%c", &ch);
+            } while (ch == 'y' || ch == 'y');
+            break;
+        case 2:
+            do 
+            {
+                printf("\nextracting the minimum key node");
+                p = bin_heap_extract_min(H);
+                if (p != NULL)
+                    printf("\nthe extracted node is %d", p->n);
+                printf("\nnow the heap is:\n");
+                display(H);
+                printf("\nextract more(y/y)\n");
+                fflush(stdin);
+                scanf("%c", &ch);
+            } while (ch == 'y' || ch == 'y');
+            break;
+        case 3:
+            do 
+            {
+                printf("\nenter the key of the node to be decreased:");
+                scanf("%d", &m);
+                printf("\nenter the new key : ");
+                scanf("%d", &l);
+                bin_heap_decrease_key(H, m, l);
+                printf("\nnow the heap is:\n");
+                display(H);
+                printf("\ndecrease more(y/y)\n");
+                fflush(stdin);
+                scanf("%c", &ch);
+            } while (ch == 'y' || ch == 'y');
+            break;
+        case 4:
+            do 
+            {
+                printf("\nenter the key to be deleted: ");
+                scanf("%d", &m);
+                bin_heap_delete(H, m);
+                printf("\ndelete more(y/y)\n");
+                fflush(stdin);
+                scanf("%c", &ch);
+            } while (ch == 'y' || ch == 'y');
+            break;
+        case 5:
+            printf("\nexiting....\n");
+            break;
+        default:
+            printf("\ninvalid entry...try again....\n");
+	}
+    } while (l != 5);
 }
